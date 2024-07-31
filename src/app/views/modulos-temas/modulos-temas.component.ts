e08580888b40112ef0cf83b3b815c7793ba2c7f6
Ezequiel-Front-End <ezequielfernandes912@gmail.com>
2024-07-31 10:15:36 -0300

    adicionando o botao de importe de excel na tabela de modulos e temas

--- src/app/views/modulos-temas/modulos-temas.component.ts
+++ src/app/views/modulos-temas/modulos-temas.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ChipsModule } from 'primeng/chips';
 import { FormsModule } from '@angular/forms';
 import { TagInputModule } from 'ngx-chips';
+import * as XLSX from "xlsx";
+import { JsonPipe } from '@angular/common';
 
 export interface AutoCompleteModel {
   value: any;
@@ -11,11 +13,13 @@ export interface AutoCompleteModel {
 @Component({
   selector: 'app-modulos-temas',
   standalone: true,
-  imports: [ChipsModule, FormsModule, TagInputModule],
+  imports: [ChipsModule, FormsModule, TagInputModule, JsonPipe],
   templateUrl: './modulos-temas.component.html',
   styleUrl: './modulos-temas.component.scss'
 })
 export class ModulosTemasComponent implements OnInit {
+
+  ExcelData: any;
 
   constructor() {
 
@@ -31,5 +35,23 @@ export class ModulosTemasComponent implements OnInit {
 
   values: string[] | any;
 
+  ReadExcel($event: any) {
+
+    let file = $event.target.files[0];
+
+    let fileReader = new FileReader();
+    fileReader.readAsArrayBuffer(file);
+
+    fileReader.onloadend = (e) => {
+      let workBook = XLSX.read(fileReader.result, { type: 'binary' });
+      var sheetNames = workBook.SheetNames;
+
+     this.ExcelData = XLSX.utils.sheet_to_json(workBook.Sheets[sheetNames[0]]);
+
+      console.log(this.ExcelData);
+
+    }
+
+  }
 
 }
